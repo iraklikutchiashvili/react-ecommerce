ca5336f18a085ca35ab0d82df35a6088a14bea7f
iraklikutchiashvili <irakli9823@gmail.com>
2023-03-14 01:51:04 +0400

    Create Checkout component for basket page

--- src/components/MyBasket.js
+++ src/components/MyBasket.js
@@ -2,10 +2,11 @@ import { Card, CardContent } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react'
 import BasketCard from "./BasketCard"
+import Checkout from './Checkout';
 
 const MyBasket = () => {
     return (
-        <Box sx={{display: "flex", gap: 5, flexDirection: "column", maxWidth: 800, width: "100%", margin: "200px auto"}}>
+        <Box sx={{display: "flex", gap: 5, flexDirection: "column", maxWidth: 800, width: "100%", margin: "150px auto"}}>
             <Card>
                 <CardContent >
                     <BasketCard/>
@@ -21,6 +22,17 @@ const MyBasket = () => {
                     <BasketCard/>
                 </CardContent>
             </Card>
+            <Card >
+                <CardContent >
+                    <BasketCard/>
+                </CardContent>
+            </Card>
+            <Card >
+                <CardContent >
+                    <BasketCard/>
+                </CardContent>
+            </Card>
+            <Checkout/>
         </Box>
     )
 }
