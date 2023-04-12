948724fc632b0d29f95e5885d5c28417f8b26513
iraklikutchiashvili <irakli9823@gmail.com>
2023-04-12 23:02:54 +0400

    Form Validation

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,9 +1,35 @@
-import React from 'react'
-import Box from '@mui/material/Box';
+import React, { useState } from 'react'
+import ErrorIcon from '@mui/icons-material/Error';
 import TextField from '@mui/material/TextField';
 import { Button, Card } from '@mui/material';
 
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
 function ContactForm({display}) {
+  const [inputValues, setInputValues] = useState({
+    name: "",
+    email: "",
+    msg: ""
+  })
+  const [showErr, setShowErr] = useState(false)
+  const handleInputs = (e) => {
+    const name = e.target.name;
+    const input = e.target.value;
+    setInputValues(prev => {return {...prev, [name]: input}})
+  }
+  const handleSubmit = () => {
+    if(inputValues.name.length > 0 && inputValues.msg.length > 0 && emailRegex.test(inputValues.email)){
+      showErr && setShowErr(false);
+      alert("Message has been succesfully submited!")
+      setInputValues({
+        name: "",
+        email: "",
+        msg: ""
+      })
+    } else {
+      setShowErr(true);
+    }
+  }
   return (
     <Card
       component="form"
@@ -18,15 +44,19 @@ function ContactForm({display}) {
       noValidate
       autoComplete="off"
     >
-    <TextField id="outlined-basic" label="Name" variant="outlined" />
-    <TextField id="outlined-basic" label="Email" variant="outlined" />
+    <div style={{color: "#cc0000", display: showErr ? "block" : "none",}}> <ErrorIcon fontSize='0.7rem'/> <span style={{fontSize: "1rem"}}>Please Provide Valid Inputs!</span></div>
+    <TextField onChange={handleInputs} id="outlined-basic" label="Name" variant="outlined" name="name" value={inputValues.name}/>
+    <TextField onChange={handleInputs} id="outlined-basic" label="Email" variant="outlined" name='email' value={inputValues.email}/>
     <TextField
+          onChange={handleInputs}
           id="outlined-multiline-static"
           label="Message"
           multiline
           rows={4}
+          name="msg"
+          value={inputValues.msg}
     />
-    <Button variant="contained" >Send</Button>
+    <Button onClick={handleSubmit} variant="contained" >Send</Button>
     </Card>
   )
 }
